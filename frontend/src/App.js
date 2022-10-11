c7e6a47e4b03a358717864f43be120d52b4059ec
Jahid-Iqbal <jahidiqbal1994@gmail.com>
2022-10-11 11:39:51 +0600

    Implementing the useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import React from "react";
+import { useState } from 'react';
+
+
+
 const App= ()=> {
+  const [word,setWord]=useState('');
+
+  const handleSearchSubmit=(e)=>{
+    e.preventDefault();
+    console.log(word);   {/*target[0] is the search bar */}
+  }
+
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search />
+      <Search word={word} setWord={setWord} handleSearch={handleSearchSubmit}/>   {/*passing the function as parameter */}
     </div>      //passing the parameter to the Header function
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({word, setWord, handleSearch}) => {
   return (
     <Container className="mt-4">
       {" "}
       {/*className="mt-4" => mt is margin top*/}
       <Row className="justify-content-center">
-        <Col xs={12} md={8} lg={6}>
+        <Col xs={12} md={8} lg={6}>     {/*xs=extra small, md=medium, lg=large. total col takes space of 12*/}
+          <Form onSubmit={handleSearch}>    
           <Row>
-            <Col xs={9}>
-              <Form.Control placeholder="Search for new images" />
+            <Col xs={9}>        {/*search bar takes space pf 9 */}
+              <Form.Control 
+                type="text"
+                value={word}
+                onChange={(e)=>setWord(e.target.value)}
+                placeholder="Search for new images" 
+            />
             </Col>
             <Col>
               <Button variant="primary" type="submit">
@@ -18,6 +24,7 @@ const Search = () => {
               </Button>
             </Col>
           </Row>
+          </Form>
         </Col>
       </Row>
     </Container>
