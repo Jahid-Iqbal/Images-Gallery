7469e0ba825d8142a2816e38d13d137c6fd59898
Jahid-Iqbal <jahidiqbal1994@gmail.com>
2022-10-12 11:41:13 +0600

    Implementing scripts in package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
-import React from "react";
+import React from 'react';
 import { useState } from 'react';
 
-const UNSPLASH_KEY= process.env.REACT_APP_UNSPLASH_KEY;   {/*connecting to upsplash API */}
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+{
+  /*connecting to upsplash API */
+}
 
-const App= ()=> {
-  const [word,setWord]=useState('');
+const App = () => {
+  const [word, setWord] = useState('');
 
-  const handleSearchSubmit=(e)=>{
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);   {/*target[0] is the search bar */}
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((result)=>result.json())
-    .then((data)=>{
-      console.log(data);
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
-    setWord('');          {/*set the default text in search bar after pressing search button*/}
+    console.log(word);
+    {
+      /*target[0] is the search bar */
+    }
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setWord('');
+    {
+      /*set the default text in search bar after pressing search button*/
+    }
+  };
+  {
+    /*fetch is a function that retrieve the photos. That is a promise. */
   }
-      {/*fetch is a function that retrieve the photos. That is a promise. */}
   return (
     <div>
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSearch={handleSearchSubmit}/>   {/*passing the function as parameter */}
-    </div>      //passing the parameter to the Header function
+      <Header title="Images Gallery" />
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSearch={handleSearchSubmit}
+      />{' '}
+      {/*passing the function as parameter */}
+    </div> //passing the parameter to the Header function
   );
-}
+};
 
 export default App;
